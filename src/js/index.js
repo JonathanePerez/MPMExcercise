a3a7fd83dc921da47e837069c0febf95093a7bf2
Jphendrix51 <jonathan.e.perez@gmail.com>
2019-07-11 14:54:14 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,29 +4,33 @@ import "../style/index.scss";
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  */
-function render(variablesz = {}) {
+function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variablesz.background}" /></div>`;
-  let githubLink = `${variablesz.github}`;
-  if (variablesz.includeCover == false) cover = "<div class='cover'></div>";
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${window.variablesz.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <img src="${window.variables.avatarURL}" class="photo" />
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
-              variablesz.twitter
+              variables.twitter
             }"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${githubLink}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/${
-              variablesz.linkedin
+              variables.linkedin
             }"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -36,27 +40,27 @@ function render(variablesz = {}) {
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function() {
-  window.variablesz = {
+  window.variables = {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/women/45.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "left",
+    socialMediaPosition: "",
     // social media usernames
-    twitter: "PaolaCastro",
-    github: "helloflatworld",
-    linkedin: "Naila",
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
+    name: "",
+    lastname: "",
+    role: "",
+    country: "",
+    city: ""
   };
-  render(window.variablesz);
+  render(window.variables);
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
       const attribute = e.target.getAttribute("for");
@@ -69,7 +73,7 @@ window.onload = function() {
             : this.value == "false"
               ? false
               : this.value;
-      render(Object.assign(window.variablesz, values));
+      render(Object.assign(window.variables, values));
     });
   });
 };
